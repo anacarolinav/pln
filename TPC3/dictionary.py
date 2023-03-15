167d58a599b8b72b3bccb8126438c7b029e76cd8
anacarolinav <107410119+anacarolinav@users.noreply.github.com>
2023-03-15 20:18:10 +0000

    Dicionario realizado com sucesso

--- TPC3/dictionary.py
+++ TPC3/dictionary.py
@@ -10,14 +10,37 @@ import re
 file=open('/Users/anacarolinaalves/Desktop/MESTRADO/2/PLN/aula0903/dicionario_medico.txt', encoding= "utf-8")
 text = file.read()
 
+#remove (1 sequencia pode conter novas linhas que começa apos \n\n\f e termina antes de \n\n) 
+#adicionar \n + 1ºgrupo de captura (.+\n\n) = todas as linhas de texto ate a ultima linha que contem 2 \n consecutivos (fim de paragrafo) 
+remove_n_f_n = re.sub(r"\n\n\f(.+\n\n)", r"\n\1", text)
 
-remove_form_feed = re.sub(r"\f", "", text)
-mark_terms = re.sub(r"\n\n(.+)", r"\n\n#T=\1", remove_form_feed)
+#captura e retira sequencia que começa apos \n\n\f e termina com uma letra [(?:) --> significa que este grupo de captura não deve ser considerado na contagem]
+#vai adicionar neste local \n\n + 1º grupo de captura ((?:.+)\n[A-ZÁÀÉÚÍÓÂÃÕ]) = \n seguido de um conjunto de caracteres. Util para capturar bloco de texto
+remove_f_string = re.sub(r"\n\n\f((?:.+)\n[A-ZÁÀÉÚÍÓÂÃÕ])", r"\n\n\1", remove_n_f_n)
+
+#substitui a sequencia \n\n\f por apenas 1 \n, ou seja, remove o caracter de form-feed e une os paragrafos num unico texto
+remove_f_2n = re.sub(r"\n\n\f", r"\n", remove_f_string)
+
+#substitui o form-feed (\f) por uma string vazia
+remove_f = re.sub(r"\f","",remove_f_2n)
+
+
+#identificamos os termos/Designações
+mark_terms = re.sub(r"\n\n(.+)", r"\n\n#T=\1", remove_f)
+
+#identificamos as explicações/Descrições
 mark_explications = re.sub(r"\n([^#\s].+)", r"\n#E=\1", mark_terms)
+
+#identifica a sequencia: termo \n\n termo como uma explicação
 correct_explications = re.sub(r"(#T=.+)\n\n#T=(.+)", r"\1\n#E=\2", mark_explications)
+
+#substitui o \n que poderá existir entre 2 explicações por um espaço.
 remove_new_lines = re.sub(r"(#E=.+)(?:\n#E=(.+))+", r"\1 \2", correct_explications)
+
+#substitui as marcas de identificação por vazio -> junta termo e explicação
 remove_marks = re.sub(r"#[TE]=", r"", remove_new_lines)
 
+#retorna uma lista de tuplos na forma (termo, explicação)
 new_entries = re.findall(r"\n\n(.+)\n(.+)", remove_marks)
 
 
@@ -32,7 +55,7 @@ header = '''
 <meta charset='utf-8'/>
 <title> Dicionário Médico </title>
 </head>
-<body style="background-color:beige;">
+<body style="background-color:white;">
 <h1 style="text-align:center;"> <br> Welcome to my medical dictionary! </h1>           
 <p style="text-align:center;">This file is organized by designation and the description on the side. <br></p> 
 
@@ -42,14 +65,14 @@ lista=list(new_entries)
 lista.insert(0,("Designation", "Description"))
 
 # Create the HTML table
-table = "<table border='1'>\n"
+table = "<table style='border: 1px solid black;border-collapse: collapse;'>\n"
 for i, row in enumerate(lista):
     table += "<tr>"
     for j, item in enumerate(row):
         if i == 0: 
-            table += "<th style='font-weight:bold';>{}</th>".format(item)
+            table += "<th style='font-weight:bold; border: 1px solid black;border-collapse: collapse;background-color: #4169e1;';>{}</th>".format(item)
         else:
-            table += "<td>{}</td>".format(item)
+            table += "<td style='border: 1px solid black;border-collapse: collapse;'>{}</td>".format(item)
     table += "</tr>\n"
 table += "</table>"
 
